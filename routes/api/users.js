390ce92c2ae1c418a5d2715d9d7308bb96c2022f
Daniel <d.chen323@gmail.com>
2018-07-20 12:55:43 -0700

    Create working login function

--- routes/api/users.js
+++ routes/api/users.js
@@ -90,7 +90,11 @@ router.post('/login', (req, res) => {
 //@desc return current user
 //@access private route
 router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) =>{
-  res.json({msg: 'Success'});
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  });
 });
 
 module.exports = router;
