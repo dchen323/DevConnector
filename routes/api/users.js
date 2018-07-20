6540761bd0c9ea5dc15bea1fd703f13c79c529a8
Daniel <d.chen323@gmail.com>
2018-07-20 12:30:47 -0700

    Create user login token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
+const keys = require('../../config/key');
 
 // @route GET api/users/test
 //@desc Tests posts route
@@ -64,7 +66,17 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if(isMatch){
-            res.json({msg: 'Success'});
+            //User Matched
+
+            //create jwt payload
+            const payload = { id: user.id, name: user.name, avatar: user.avatar };
+            //Sign token
+            jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err,token) => {
+              res.json({
+                success: true,
+                token: `Bearer ${token}`
+              });
+            });
           }else{
             return res.status(400).json({password: 'Incorrect username/password'});
           }
