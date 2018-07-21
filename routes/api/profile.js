f5853f2903919b01f1b242ca995e1d335a591426
Daniel <d.chen323@gmail.com>
2018-07-21 13:36:39 -0700

    Create routes to find users by handle and user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -31,6 +31,43 @@ router.get('/', passport.authenticate('jwt', { session: false}), (req, res) => {
     })
     .catch(err => res.status(404).json(err));
 });
+// @route POST api/handle/:handle
+//@desc Get profile by handle
+//@access public route
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if(!profile){
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route POST api/profile/user/:user_id
+//@desc Get profile by user ID
+//@access public route
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if(!profile){
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json({profile: 'There is no profile for this user'}));
+});
 
 // @route POST api/profile
 //@desc Create/edit user profile
