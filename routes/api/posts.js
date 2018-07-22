f0d1d53e5e16ce075be3662e5c95ad0003e5a941
Daniel <d.chen323@gmail.com>
2018-07-22 13:59:31 -0700

    Create get post by id and all route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -14,6 +14,25 @@ import validatePostInput from '../../validation/post';
 //@access public route
 router.get('/test', (req, res) => res.json({msg: "Posts Works"}));
 
+// @route GET api/posts
+//@desc Get Post
+//@access public route
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({nopostsfound: 'No posts found'}));
+});
+
+// @route GET api/posts/:id
+//@desc Get Post by id
+//@access public route
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({nopostfound: 'No post found with that ID'}));
+});
+
 // @route POST api/posts
 //@desc Create Post
 //@access private route
