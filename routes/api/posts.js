486d34239320c5d07782fa35e4706d31be6cc604
Daniel <d.chen323@gmail.com>
2018-07-22 14:18:34 -0700

    Create like and unlike routes for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -75,4 +75,51 @@ router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, re
     });
 });
 
+// @route POST api/posts/like/:id
+//@desc Like Post
+//@access private route
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+            return res.status(400).json({ alreadyliked: 'User already liked this post'});
+          }
+
+          //Add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+    });
+});
+
+// @route DELETE api/posts/like/:id
+//@desc Unlike Post
+//@access private route
+router.delete('/like/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+            return res.status(400).json({ notliked: 'You have not liked this post'});
+          }
+
+          //Get remove index
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+          //splice out of array
+          post.likes.splice(removeIndex, 1);
+
+          //save post
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+    });
+});
+
 export default router;
