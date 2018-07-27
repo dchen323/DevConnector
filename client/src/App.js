58cca584af47122927eeb4a0a99de479731bcd63
Daniel <d.chen323@gmail.com>
2018-07-26 22:31:10 -0700

    Create profile creating component

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/createProfile/CreateProfile";
 
 //Check for Token
 if (localStorage.jwtToken) {
@@ -48,6 +49,13 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
