4fb3f3accfcaa08d10a2b9136d5cd50338b4247b
Daniel <d.chen323@gmail.com>
2018-07-29 11:49:24 -0700

    Create education table and allow for education to be added and deleted

--- client/src/components/editProfile/EditProfile.js
+++ client/src/components/editProfile/EditProfile.js
@@ -6,7 +6,7 @@ import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import SelectListGroup from "../common/SelectListGroup";
 import InputGroup from "../common/InputGroup";
 import { createProfile, getCurrentProfile } from "../../actions/profileActions";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 import isEmpty from "../../validation/isEmpty";
 
 class EditProfile extends Component {
@@ -208,6 +208,9 @@ class EditProfile extends Component {
         <div className="container">
           <div className="row">
             <div className="col-mid-8 m-auto">
+              <Link to="/dashboard" className="btn btn-light">
+                Go Back
+              </Link>
               <h1 className="display-4 text-center">Edit your Profile</h1>
               <small className="d-block pb-3">* = required fields</small>
               <form onSubmit={this.onSubmit}>
