b6de614e8c9d82ef5705d50e483919ba39dfa4a1
Daniel <d.chen323@gmail.com>
2018-07-29 12:54:17 -0700

    Create profile component

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import ProfileAbout from "./ProfileAbout";
+import ProfileCreds from "./ProfileCreds";
+import ProfileGithub from "./ProfileGithub";
+import ProfileHeader from "./ProfileHeader";
+import Spinner from "../common/Spinner";
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <ProfileHeader />
+        <ProfileAbout />
+        <ProfileCreds />
+        <ProfileGithub />
+      </div>
+    );
+  }
+}
