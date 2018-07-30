8d6f1890101102c10d76b18c5c4de33a18c2b334
Daniel <d.chen323@gmail.com>
2018-07-30 12:30:27 -0700

    Allow comments on individual posts

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Spinner from "../common/Spinner";
 import PostItem from "../posts/PostItem";
+import CommentForm from "./CommentForm";
 import { getPost } from "../../actions/postActions";
 
 class Post extends Component {
@@ -20,6 +21,7 @@ class Post extends Component {
       postContent = (
         <div>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
         </div>
       );
     }
